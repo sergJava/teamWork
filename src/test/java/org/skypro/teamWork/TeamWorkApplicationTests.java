3abf6b742fe9e869bd42fea91b573273a8f1679f
sergf <sergfibih@ya.ru>
2025-07-06 17:12:01 +1000

    initial commit

---
+++ src/test/java/org/skypro/teamWork/TeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.teamWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
